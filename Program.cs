5bdcd86b3a7c087b8cb0589587f5788eb136745b
willpower18 <willjcpower@gmail.com>
2022-04-22 08:48:45 -0300

    Colocando peças no tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using Jogo.Tabuleiro;
-
+using Jogo.Xadrez;
 
 Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+
+tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta) ,new Posicao(0,0));
+tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta) ,new Posicao(1,3));
+tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta) ,new Posicao(2,4));
 
 Tela.ImprimirTabuleiro(tabuleiro);
 
